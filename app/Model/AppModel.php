c1c8289a833efc3807814c02ab0648b769047913
Remy <remy@stripthis.com>
2012-01-22 19:48:54 +0530

    adding alphaplus generic rule

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -17,5 +17,18 @@ class AppModel extends Model {
    * @var integer
    */
   public $recursive = -1;
-
+  
+  /**
+   * Validation Rule, check if the string is a word 
+   * It allows one dashe or space in between word section
+   * @param $data array is passed using the form field name as the key
+   * @return bool
+   */
+  function alphaplus($check) {
+    // have to extract the value to make the function generic
+    $value = array_values($check);
+    $value = $value[0];
+    // @TODO support unicode characters => pb in JS
+    return preg_match('/^((\w)+(\-|\s){1})+(\w)+$/', $value);
+  }
 }//_EOF
