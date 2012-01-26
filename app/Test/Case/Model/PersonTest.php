048c1b50978c869e0cb829a794e0ebc5325b8022
Remy <remy@stripthis.com>
2012-01-27 00:34:10 +0530

    tests

--- app/Test/Case/Model/PersonTest.php
+++ app/Test/Case/Model/PersonTest.php
@@ -1,6 +1,14 @@
 <?php
+/**
+ * Person Model Test
+ *
+ * @package     app.Test.Model.Person
+ * @copyright   Copyright 2012, ActionAid Association India 
+ * @link        http://actionaid.org/india
+ * @author      Remy Bertot / Kevin Muller
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 App::uses('Person', 'Model');
-
 class GiftTestCase extends CakeTestCase {
 
   public function setup() {
@@ -8,30 +16,107 @@ class GiftTestCase extends CakeTestCase {
     $this->Person = ClassRegistry::init('Person');
   }
 
-  public function testPersonAgeValidation() {    
-    $fixtures = array(
-      'Ms' => true,   'ms' => false,
-      'Mrs' => true,  'mrs' => false,
-      'Mr' => true,   'mr' => false,
-      'Dr' => true,   'dr' => false,
-      'Prof' => true, 'prof' => true,
-      '#' => false,   '1' => false,
-      'biloute' => false
+  public function testDobValidation() {    
+    $testcases = array(
+      '2000-01-01' => false, '1800-01-01'  => false,
+      '1990-01-01' => true,  '1960-01-01'  => true
     );
-    foreach($fixtures as $fixture => $result) {
+    foreach($testcases as $testcase => $result) {      
+      $person = array('Person' => array(
+        'dob' => $testcase
+      ));
       if($result) {
-        $msg = 'validation of person title with '.$fixture.' should validate';
+        $msg = 'validation of person dob with '.$testcase.' should validate';
       } else {
-        $msg = 'validation of person title with '.$fixture.' should not validate';
+        $msg = 'validation of person dob with '.$testcase.' should not validate';
       }
       $this->assertEqual(
-        $this->Person->validates(array('fieldList' => array('city'))), $result, $msg
+        $this->Person->validates(array('fieldList' => array('dob'))), $result, $msg
       );
     }
   }
 
+  public function testAdultAgeValidation() {    
+    $testcases = array(
+      '2009-01-01' => false, '1800-01-01'  => true,
+      '1990-01-01' => true,  '1960-01-01'  => true
+    );
+    foreach($testcases as $testcase => $result) {  
+      if($result) {
+        $msg = 'validation of person age with '.$testcase.' should validate';
+      } else {
+        $msg = 'validation of person age with '.$testcase.' should not validate';
+      }
+      $this->assertEqual($this->Person->isAnAdult(array('dob' => $testcase)), $result, $msg);
+    }
+  }
+
+  public function testStateValidation() {
+    $testcases = array(
+      'IN-AP' => true, 
+      'AP-IN' => false, 
+      'XX-IN' => false 
+    );
+    foreach($testcases as $testcase => $result) {      
+      $person = array('Person' => array(
+        'state' => $testcase
+      ));
+      $this->Person->set($person);
+      if($result) {
+        $msg = 'validation of state with '.$testcase.' should validate';
+      } else {
+        $msg = 'validation of state with '.$testcase.' should not validate';
+      }
+      $this->assertEqual($this->Person->validates(array('fieldList' => array('state'))), $result, $msg);
+      $this->assertEqual($this->Person->validateState(array('state' => $testcase)), $result, $msg);
+    }
+  }
+
+  public function testCountryValidation() {
+    $testcases = array(
+      'IN' => true, 
+      'XX' => false, 
+      'XWX' => false 
+    );
+    foreach($testcases as $testcase => $result) {      
+      $person = array('Person' => array(
+        'country' => $testcase
+      ));
+      $this->Person->set($person);
+      if($result) {
+        $msg = 'validation of country with '.$testcase.' should validate';
+      } else {
+        $msg = 'validation of country with '.$testcase.' should not validate';
+      }
+      $this->assertEqual($this->Person->validates(array('fieldList' => array('country'))), $result, $msg);
+      $this->assertEqual($this->Person->validateCountry(array('country' => $testcase)), $result, $msg);
+    }
+  }
+
+  public function testGetFindConditions() {
+    $testcase = array( 
+      'Person' => array(
+        'title'     => 'Mr', 
+        'dob'       => array('year'=>'1981','month'=>'01','day'=>'01'),
+        'firstname' => 'test',
+        'lastname'  => 'test',
+        'address1'  => 'test',
+        'address2'  => 'test',
+        'city'      => 'test',
+        'pincode'   => '123456',
+        'state'     => 'IN-AP',
+        'country'   => 'IN',
+        'email'     => 'test@test.com',
+        'phone'     => '111111111',
+        'pan'       => 'AAAAA9999A'
+      )
+    );
+    $results = $this->Person->getFindConditions('findDuplicates',$testcase);
+    $this->assertEqual(sizeof($results),sizeof($testcase['Person']));
+  }
+
   public function testPersonTitleValidation() {
-    $fixtures = array(
+    $testcases = array(
       'Ms' => true,   'ms' => false,
       'Mrs' => true,  'mrs' => false,
       'Mr' => true,   'mr' => false,
@@ -40,11 +125,15 @@ class GiftTestCase extends CakeTestCase {
       '#' => false,   '1' => false,
       'biloute' => false
     );
-    foreach($fixtures as $fixture => $result) {
+    foreach($testcases as $testcase => $result) {
+      $person = array('Person' => array(
+        'title' => $testcase
+      ));
+      $this->Person->set($person);
       if($result) {
-        $msg = 'validation of person title with '.$fixture.' should validate';
+        $msg = 'validation of person title with '.$testcase.' should validate';
       } else {
-        $msg = 'validation of person title with '.$fixture.' should not validate';
+        $msg = 'validation of person title with '.$testcase.' should not validate';
       }
       $this->assertEqual(
         $this->Person->validates(array('fieldList' => array('title'))), $result, $msg
@@ -53,23 +142,30 @@ class GiftTestCase extends CakeTestCase {
   }
 
   public function testPersonCityValidation() {
-    $fixtures = array(
+    $testcases = array(
       '#' => false,
-      'd' => true,
+      'd' => false,
+      '22' => false,
+      'd22' => false,
       'delhi' => true,
+      'rémy' => false,
       'delhi-ji' => true,
+      "delhi'ji" => true,
       'Dehri-on-Sone' => true,
-      "delhi'ji-ji" => true
+      "delhi'ji-ji" => true,
+      'Dehri-on-Sone-' => false,
+      '-Dehri-on-Sone' => false,
+      'Dehri-on--Sone' => false
     );
-    foreach($fixtures as $fixture => $result) {
+    foreach($testcases as $testcase => $result) {
       $person = array('Person' => array(
-        'city' => $fixture
+        'city' => $testcase
       ));
       $this->Person->set($person);
       if($result) {
-        $msg = 'validation of city name with '.$fixture.' should validate';
+        $msg = 'validation of city name with '.$testcase.' should validate';
       } else {
-        $msg = 'validation of city name with '.$fixture.' should not validate';
+        $msg = 'validation of city name with '.$testcase.' should not validate';
       }
       $this->assertEqual(
         $this->Person->validates(array('fieldList' => array('city'))), $result, $msg
