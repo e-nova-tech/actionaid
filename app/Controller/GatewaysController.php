380e295a4edbd266424fac234d331c12282e3f7d
kevin <mynameisgnu@gmail.com>
2012-02-10 16:41:46 +0530

    Payment Gateway classes

---
+++ app/Controller/GatewaysController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Gateways Controller
+ *
+ * @package     app.Controller.GatewaysController
+ * @copyright   Copyright 2012, ActionAid Association India
+ * @link        http://actionaid.org/india
+ * @author      Remy Bertot / Kevin Muller
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class GatewaysController extends AppController {
+  
+}
+
---
+++ app/Model/Gateway.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Gateway Model
+ * 
+ * @package     app.Model.Gateway
+ * @copyright   Copyright 2012, ActionAid Association India 
+ * @link        http://actionaid.org/india
+ * @author      Remy Bertot / Kevin Muller
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class Gateway extends AppModel {
+  public $name = 'Gateway';
+
+}
