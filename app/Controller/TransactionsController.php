1436e7882b9a29a36e94c73fdb484effb4561bce
kevin <mynameisgnu@gmail.com>
2012-03-01 17:39:30 +0530

    Added the prefix IPG in front of orderId

--- app/Controller/TransactionsController.php
+++ app/Controller/TransactionsController.php
@@ -95,7 +95,7 @@ class TransactionsController extends AppController {
     
     // define the transaction for the view
     $t['Transaction'] = array(
-      'orderId'    => $request['Transaction']['serial'], // orderId is the transaction serial
+      'orderId'    => 'IPG'.$request['Transaction']['serial'], // orderId is the transaction serial
       'amount'     => $gift['Gift']['amount'],
       'currency'   => $gift['Gift']['currency'],
       'paymentUrl' => $pg['paymentUrl'],
